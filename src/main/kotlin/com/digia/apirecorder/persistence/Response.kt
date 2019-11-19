9c557141d067ed8aad7a419eb614c7bacd99ff12
Pierre Leonard <pierre.leonard@digia.com>
2019-11-19 15:04:12 +0200

    Added response type, code and time

--- src/main/kotlin/com/digia/apirecorder/persistence/Response.kt
+++ src/main/kotlin/com/digia/apirecorder/persistence/Response.kt
@@ -8,10 +8,21 @@ data class Response(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id : Int?,
-    val body : String,
+    val body : String?,
     @ManyToOne
     @JoinColumn(name= "request_id", nullable = false)
     val request : Request,
     val timestamp: LocalDateTime,
-    @Column(name = "time_offset")
-    val timeOffset : Int)
+    @Column(name = "time_offset", nullable = false)
+    val timeOffset : Int,
+    @Enumerated(EnumType.STRING)
+    val type: ResponseType,
+    @Column(name = "response_code", nullable = false)
+    val responseCode : Int?,
+    @Column(name = "response_time", nullable = false)
+    val responseTime : Long)
+
+enum class ResponseType{
+    NEW,
+    OLD
+}
--- src/main/kotlin/com/digia/apirecorder/persistence/ResponseRepository.kt
+++ src/main/kotlin/com/digia/apirecorder/persistence/ResponseRepository.kt
@@ -12,11 +12,11 @@ interface ResponseRepository : JpaRepository<Response, Long> {
 
     fun findById(id : Int) : Response?
     fun findByRequestId(requestId : Int) : List<Response>?
-    @Query("SELECT r from Response r WHERE r.request = ?1 AND r.timeOffset <= ?2 ORDER BY timeOffset DESC")
-    fun findByRequestAndTimeOffset(request: Request, timeOffset : Int, pageRequest : PageRequest) : List<Response>
+    @Query("SELECT r from Response r WHERE r.request = ?1 AND r.timeOffset <= ?2 AND r.type = 'NEW' ORDER BY timeOffset DESC")
+    fun findNewByRequestAndTimeOffset(request: Request, timeOffset : Int, pageRequest : PageRequest) : List<Response>
 }
 
 fun ResponseRepository.findTopByRequestAndTimeOffset(request : Request, timeOffset : Int): Response? {
-    val responses = findByRequestAndTimeOffset(request, timeOffset, PageRequest.of(0, 1))
+    val responses = findNewByRequestAndTimeOffset(request, timeOffset, PageRequest.of(0, 1))
     return if(responses.isNotEmpty()) responses[0] else null
 }
